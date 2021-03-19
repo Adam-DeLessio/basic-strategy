212f17f7f23545e17db007cf8fbdbdaa10ff218a
Adam <adamdelessio@gmail.com>
2021-03-19 14:12:51 -0400

    chart reveal and close buttons are working

--- chart.js
+++ chart.js
@@ -1,7 +1,10 @@
 let chartRevealBtn = document.querySelector('#chartRevealBtn')
+let body = document.querySelector('#body')
 let chartBody = document.querySelector('#chart-body')
+let chartSection = document.querySelector('#chart-section')
+let chartCloseBtn = document.querySelector('#chartCloseBtn')
 
-let topRowValues = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'A']
+let topRowValues = [null, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'A']
 let cardChart = [
 	['17+', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[16, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
@@ -15,22 +18,25 @@ let cardChart = [
 	['5-8', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+// Reveal chart button
 function revealChart() {
 	let main = document.querySelector('#main')
-	let chartSection = document.querySelector('#chart-section')
 	main.style.display = 'none'
 	chartSection.style.display = 'flex'
-
-	makeChart()
+	chartCloseBtn.style.display = 'flex'
 }
 
+// Creates the charts when the program loads
 function makeChart(playerCards) {
 	let chartBody = document.querySelector('#chart-body')
 	let topRow = document.createElement('tr')
 	chartBody.appendChild(topRow)
 	topRowValues.forEach(r => {
 		let newColumn = document.createElement('td')
-		if (r != 0) {
+		if (r === null) {
+			newColumn.style.backgroundColor = 'black'
+			topRow.appendChild(newColumn)
+		} else if (r != 0 && r != null) {
 			newColumn.innerText = r
 			newColumn.style.backgroundColor = 'white'
 			topRow.appendChild(newColumn)
@@ -40,7 +46,6 @@ function makeChart(playerCards) {
 			topRow.appendChild(newColumn)
 		}
 	})
-
 	cardChart.forEach(r => {
 		let newRow = document.createElement('tr')
 		chartBody.appendChild(newRow)
@@ -63,4 +68,11 @@ function makeChart(playerCards) {
 			newRow.appendChild(newColumn)
 		})
 	})
+}
+makeChart()
+
+// Close chart screen
+function closeChart() {
+	main.style.display = 'flex'
+	chartSection.style.display = 'none'
 }
